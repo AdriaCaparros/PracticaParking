7d7204f180b51e226355686a012d96ec71ff800e
adria <adria.caparros@estudiants.urv.cat>
2019-04-10 18:13:18 +0200

    Primera pantalla

---
+++ app/src/main/java/com/example/practicaparking/FirtMenuCarNav.java
@@ -0,0 +1,13 @@
+package com.example.practicaparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirtMenuCarNav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firt_menu_car_nav);
+    }
+}
